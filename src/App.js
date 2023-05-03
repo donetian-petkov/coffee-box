5a3018afecc85b7c7a686ac748e2f519b7f2f9cc
donetian-petkov <donetianpetkov@gmail.com>
2023-05-03 14:24:55 +0300

    Initial Commit, Routes, Responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {Main} from "./views/Main";
+import {ProtectedRoute} from "./components/authComponents/ProtectedRoute";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path='/' element={<Main/>}/>
+            <Route element={<ProtectedRoute user={auth} />}>
+                <Route path='/logout' element={<Logout/>}/>
+            </Route>
+            <Route path='/search/:searchWords' element={<Search />} />
+            <Route path="*" element={PageNotFound} />
+        </Routes>
+    );
 }
 
 export default App;
