6402d9837c67c347ec732577928fa5e23201b776
donetian-petkov <donetianpetkov@gmail.com>
2023-05-05 09:52:18 +0300

    Hero Page Style

--- src/components/mainComponent/Main.js
+++ src/components/mainComponent/Main.js
@@ -6,7 +6,7 @@ export const Main = () => {
 
     return (
 
-        <Card>
+        <Card className={styles.hero}>
             <Card.Img src={hero_new} alt="Card image"/>
             <Card.ImgOverlay className={styles.hero__imgOverlay}>
                 <Card.Title className={styles.hero__title}>Coffee Box</Card.Title>
@@ -16,7 +16,6 @@ export const Main = () => {
                 <Card.Text className={styles.hero__subs}>
                     Специално подбран за теб
                 </Card.Text>
-                
             </Card.ImgOverlay>
         </Card>
     )
